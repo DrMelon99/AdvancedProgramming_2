67294ce9ab6fb1c77921e4ee29a0378667b7511e
sominoh <sominoh54@gmail.com>
2023-12-05 05:30:34 +0900

    수정 내용

---
+++ jjuggumi_game/source/game_juldarigi.c
@@ -0,0 +1,34 @@
+#include "games.h"
+
+void juldarigi(void)
+{
+	juldarigi_init();
+
+	dialog(0, -1);
+	
+	system("cls");
+
+	map_init(3, 32);
+		
+	while (1) // 게임 진행 루프
+	{
+
+
+
+		display();
+		Sleep(10);
+		tick[0] += 10;
+	}
+}
+
+void juldarigi_init(void)
+{
+	game_round = 3;
+
+	system("cls");
+
+	map_init(9, 32);
+
+	SetConsoleFontSize(20);
+	system("mode con: cols=32 lines=30");
+}
