ccfd4ae31f1573b30f40177e04dfaebd02087f6c
Dr_Melon99 <jehuyk11@gmail.com>
2023-12-07 21:47:32 +0900

    ✅ JJUGGUMI GAME COMPLETE

--- jjuggumi_game/source/games.h
+++ jjuggumi_game/source/games.h
@@ -20,19 +20,25 @@
 #define MUGUNWHA_MENT "무 궁 화 꽃 이 피 었 습 니 다"
 #define NIGHTGAME_ALART1 "Player n obtained an item"
 #define NIGHTGAME_ALART2 "Player n steals an item from Player n"
+#define JULDARIGI_ALART1 "Player n fell into the hole"
+#define JULDARIGI_ALART2 "LEFT Team lay down"
+#define JULDARIGI_ALART3 "RIGHT Team lay down"
 
-bool yh_killmode, get_item;
-int fight;
-int count;
+bool yh_killmode, get_item, break_cmd;
+int fight, jebi_sel;
+int game_cycle_count;
 int x, y;
+int jul_mid;
 int ingame_exchange_data;
 
-int picked_jb, unpicked_jb;
-int jb_pf[10]; // 제비 당첨 여부
-int pl_cnt, r1; // 플레이어, 라운드 1
-int jb_num;
-int pl_now[10];
-int jb_cnt, p_cnt;
+int juldarigi_team[2][5];
+int jy[3]; // 줄 좌표
+double str_control; // 게임마스터의 줄다리기 힘 조절
+bool jul_lay_down[2]; // 게임마스터의 줄다리기 눕기 조작 여부 [L:R]
+double juldarigi_str[3]; // 0: 힘의 합, 1: 좌측 합, 2: 우측 합
+
+int jebis[10][3];// 0: PASS / FAIL 여부, 1: 개봉 여부, 2: 해당좌표
+
 
 /* 공통 영역 */
 // ★ 사용자 움직임 정의
@@ -47,16 +53,14 @@ void player_visable(void);
 void player_stamina(int p, int opt, int stamina_val);
 
 /* "무궁화 꽃이 피었습니다" 영역 */
-// ♨ "무궁화 꽃이 피었습니다" 메인
+// ★ "무궁화 꽃이 피었습니다" 메인
 void mugunghwa(void);
-// ♨ "무궁화 꽃이 피었습니다" 초기 설정
+// ★ "무궁화 꽃이 피었습니다" 초기 설정
 void mugunghwa_init(void);
-// ♨ "무궁화 꽃이 피었습니다" NPC 움직임 정의
+// ★ "무궁화 꽃이 피었습니다" NPC 움직임 정의
 void npc_move_mugunghwa(void);
-// ♨ "무궁화 꽃이 피었습니다" back_buf[][]에 움직임 반영
-void move_tail_mugunghwa(int player, int nx, int ny);
-// ♨ "무궁화 꽃이 피었습니다" 영희 행동 정의
-void younghee(void);
+// ★ "무궁화 꽃이 피었습니다" back_buf[][]에 움직임 반영
+void can_move_mugunghwa(int player, int nx, int ny);
 
 /* "야간운동" 영역 */
 // ★ "야간운동" 메인
@@ -69,22 +73,24 @@ void npc_move_nightgame(void);
 void nightgame_item_visable(void);
 
 /* "줄다리기" 영역 */
-// ♨ "줄다리기" 메인
+// ★ "줄다리기" 메인
 void juldarigi(void);
-// ♨ "줄다리기" 초기 설정
+// ★ "줄다리기" 초기 설정
 void juldarigi_init(void);
-// ♨ "줄다리기" 게임 화면 - 내가 만든 것(보류) 
-void map_init_juldarigi(int n_row, int n_col);
+// ★ "줄다리기" 게임마스터 컨트롤
+void juldarigi_master_control(void);
+// ★ "줄다리기" 줄 이동
+void juldarigi_move(void);
 
 /* "제비뽑기" 영역 */
-// ♨ "제비뽑기" 메인
+// ★ "제비뽑기" 메인
 void jebi(void);
-// ♨ "제비뽑기" 초기 설정
+// ★ "제비뽑기" 초기 설정
 void jebi_init(void);
-//제비 섞기
-void jebi_mix(int[]);
-//제비 고르는 순서 정하기
-void player_mix(int[]);
-//제비 뽑기 조작
-bool pick_jebi();
+// ★ "제비뽑기" 제비 섞기
+void jebi_mix(void);
+// ★ "제비뽑기" 제비 고르기
+bool jebi_select(void);
+
+
 #endif
